67b9b15d775c7677222473c26d1a6bed634ab2c8
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-11-10 01:20:15 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+  const links = [
+    { id: 1, val: "books" },
+    { id: 2, val: "categories" },
+  ];
+  return (
+    <div>
+      
+    </div>
+  )
+}
