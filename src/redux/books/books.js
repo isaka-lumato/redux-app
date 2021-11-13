1ad5457c22fe2da1cb52143f19ece933c304a81b
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-11-13 13:50:06 +0300

    implement api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,25 +1,67 @@
-const ADD_BOOK = 'redux-app/books/ADD_BOOK';
-const REMOVE_BOOK = 'redux-app/books/REMOVE_BOOK';
-const initialState = [];
+import * as api from '../../api/api';
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
-export const removeBook = (bookID) => ({
-  type: REMOVE_BOOK,
-  id: bookID,
-});
+const ADD_BOOK = 'BookStores/books/ADD_BOOK';
+const REMOVE_BOOK = 'BookStores/books/REMOVE_BOOK';
+const GET_BOOKS = 'BookStores/books/GET_BOOKS';
 
-const reducer = (state = initialState, action) => {
+const handleData = (data) => {
+  const books = [];
+  const keys = Object.keys(data);
+
+  keys.forEach((key, index) => {
+    const book = data[keys[index]];
+    book[0].item_id = key;
+
+    books.push(book[0]);
+  });
+
+  return books;
+};
+
+// ACTION CREATORS
+export const getBooks = () => async (dispatch) => {
+  try {
+    const data = await api.fetchBooks();
+
+    dispatch({ type: GET_BOOKS, payload: handleData(data) });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const addBook = (book) => async (dispatch) => {
+  try {
+    await api.postBook(book);
+
+    dispatch({ type: ADD_BOOK, payload: book });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const removeBook = (bookId) => async (dispatch) => {
+  try {
+    await api.deleteBook(bookId);
+
+    dispatch({ type: REMOVE_BOOK, payload: bookId });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// REDUCER
+
+const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
+    case GET_BOOKS:
+      return action.payload;
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
+      return state.filter((book) => book.item_id !== action.payload);
     default:
       return state;
   }
 };
-
+// export default reducer;
 export default reducer;
